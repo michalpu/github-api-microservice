481ecdefbd600e3de37132be31b5e42e5134d1dd
michalpu <michalpurtak1@gmail.com>
2018-04-28 12:52:50 +0200

    First commit

---
+++ zadanie-allegro-staz/src/main/java/com/michalpu/zadanieallegrostaz/ZadanieAllegroStazApplication.java
@@ -0,0 +1,12 @@
+package com.michalpu.zadanieallegrostaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieAllegroStazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieAllegroStazApplication.class, args);
+	}
+}
